8718394e9ddec788a59a4e1dd239b1d3b4913bfc
bighsueh <ss313021490@gmail.com>
2024-04-14 00:36:23 +0800

    edit(Individual): 1. 更正性別的變數名稱 male -> gender

--- Individual.py
+++ Individual.py
@@ -3,9 +3,9 @@ from typing import List
 from Coord import Coord
 
 class Individual():
-    def __init__(self) -> None:
+    def __init__(self, id:int, gender:str, age:int, intro:str, habit:List[str], coord:Coord) -> None:
         self.id: int
-        self.male: str
+        self.gender: str
         self.age: int
         self.intro: str
         self.habits: List[str]
