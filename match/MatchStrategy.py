e3a3f12d043d6a17fe1224e2b8e40cf6378ac659
bighsueh <ss313021490@gmail.com>
2024-04-14 00:38:18 +0800

    add(MatchStrategy): 1. 加入 match interface

---
+++ match/MatchStrategy.py
@@ -0,0 +1,9 @@
+from Individual import Individual
+from typing import List
+
+class MatchStrategy():
+    def __init__(self) -> None:
+        pass
+    
+    def match(self, candidates: List[Individual], individual: Individual) -> List[Individual]:
+        pass
